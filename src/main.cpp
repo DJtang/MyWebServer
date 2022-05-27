54e585a26f6eef06ffdcae8b977e0ce126f8f914
Cat like fish <tangfeng.email@qq.com>
2022-05-27 17:14:06 +0800

    修改

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
+/*
+ * @Author       : DJtang
+ * @Date         : 2022-01-23
+ * @copyleft GPL 2.0
+ */
+
 #include <iostream>
+#include "webserver.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
--- src/server/webserver.h
+++ src/server/webserver.h
@@ -2,13 +2,30 @@
 // Created by 11240 on 2022/5/25.
 //
 
-#ifndef MYWEBSERVER_WEBSERVER_H
-#define MYWEBSERVER_WEBSERVER_H
+#ifndef WEBSERVER_H
+#define WEBSERVER_H
+
+#include <assert.h>
+#include <errno.h>
+#include <unordered_map>
+#include <fcntl.h>
+#include <unistd.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
 
 
-class webserver {
+
+class Webserver {
+//public:
+//
+//private:
+//    bool InitSocket();
+//    void InitEventMode(int trigMode);
+//    void AddClient(int fd , sockaddr_in addr);
+
 
 };
 
 
-#endif //MYWEBSERVER_WEBSERVER_H
+#endif //WEBSERVER_H
