b027d089b6d919f7c6fd366df0a3db222500039c
wells <tangfeng.email@qq.com>
2022-06-18 17:14:38 +0800

    添加了一些本地文件

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,24 @@
  * @copyleft GPL 2.0
  */
 
-#include <iostream>
-#include "webserver.h"
+#include <unistd.h>
+#include "server/webserver.h"
 
 int main() {
-    Webserver* obj = new Webserver;
-    obj->print();
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    /* 守护进程 后台运行 */
+    //daemon(1, 0);
+//    int pid;
+//    for(int i=0;i<5;i++){
+//        if(pid=fork()){
+//            if(i==4) exit(0);
+//            continue;
+//        }
+//        else break;
+//    }
+    WebServer server(
+            55556, 1, 60000, false,             /* 端口 ET模式 timeoutMs 优雅退出  */
+            3306, "root", "123456", "mydb", /* Mysql配置 */
+            12, 8, true, 3, 1024);             /* 连接池数量 线程池数量 日志开关 日志等级 日志异步队列容量 */
+    server.Start();
 }
+
